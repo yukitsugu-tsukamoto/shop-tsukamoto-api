e632fbde23ede41535900c96b7fcbe7aa3cacd42
yukitsugu-tsukamoto <yukitsugu.tsukamoto@gmail.com>
2017-07-12 10:29:09 +0900

    aplication.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,7 +48,7 @@ end
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/secrets.yml'
+append :linked_files, 'config/database.yml', 'config/secrets.yml', 'config/application.yml'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
