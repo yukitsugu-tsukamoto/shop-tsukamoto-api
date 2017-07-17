227da66dfa0db354b66367442b81ee986ecbb940
yukitsugu-tsukamoto <tsukamoto@ytsukamoto.local>
2017-07-17 17:27:02 +0900

    辻中対応

--- app/controllers/api/v1/welcome_controller.rb
+++ app/controllers/api/v1/welcome_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::WelcomeController < ApplicationController
   def index
+    if params['user_name'] == 'tsujinaka'
+      render json: {response: "<@#{params['user_id']}> あ？"}
+    end
+
     text = ''
     search_word_tmp = params['text'].match(/(ほしい|がほしい|欲しい|が欲しい)$/)
     if search_word_tmp.present?
